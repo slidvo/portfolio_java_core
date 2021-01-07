4ce49785e845f37e09bb3101629caa04836598e1
slidvo <ks031088@gmail.com>
2021-01-07 13:51:31 +0500

    Refactor of Main.class

--- src/portfolio_java_core/Main.java
+++ src/portfolio_java_core/Main.java
@@ -22,18 +22,17 @@ import examples.WorkWithStrings;
 import examples.WrittingFiles;
 
 public class Main {
-	
+
 	private static final Logger logger = LogManager.getLogger(Main.class);
+	// Creating instance of TypeOfData
+	private static TypesOfData tod = new TypesOfData();
+	private static WorkWithStrings wws = new WorkWithStrings();
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Main class was started");
 
-		// Creating instance of TypeOfData
-		TypesOfData tod = new TypesOfData();
 		// show initialized variables inside class
 		tod.showVariables();
-
-		WorkWithStrings wws = new WorkWithStrings();
 
 		wws.showMe();
 		wws.showStringMethods();
@@ -44,10 +43,39 @@ public class Main {
 
 		new HandlingExceptions();
 
-		StaticElements.staticMethod(StaticElements.staticVariable);
+		
+		//We can execute static method without creating instance of class
+		StaticElements.staticMethod(StaticElements.staticVariable); 
 
 		new InheritanceExmpl("Slava", "SpaceX");
+		
+		//Examples of work with different extensions files (reading writing)
+		workWithFiles();
+		
 
+		// CollectionsExample
+		new CollectionsExample();
+
+		// HashSet example
+		new HashSetExample();
+
+		// LinkedList example
+		new LinkedListExample();
+
+		// Log4j
+		logger.trace("Entering application.");
+
+		Log4jExample l4 = new Log4jExample();
+
+		if (!l4.doIt()) {
+			logger.error("Did not do it.");
+		}
+		logger.trace("Exiting applicatiopn.");
+
+		System.out.println("Main class finished the exection ");
+	}
+
+	static void workWithFiles() throws IOException {
 		ReadingConfigurationsFromFile rcff = new ReadingConfigurationsFromFile("object.properties");
 		rcff.showAllProperties();
 
@@ -59,60 +87,27 @@ public class Main {
 		ReadingFiles read2 = new ReadingFiles("read2.csv");
 		read2.show();
 		read2.close();
-		
-		//Writing files
+
+		// Writing files
 		WrittingFiles textFile1 = new WrittingFiles("textFile1.txt");
-		
+
 		String data = "String : data for writting textFile1.txt";
-		
+
 		textFile1.write(data);
 		textFile1.close();
-		
-		data = "<!doctype html>"
-				.concat("<html>")
-				.concat("<head>")
-				.concat("<meta charset=\"utf-8\">")
-				.concat("<title>htmlFile</title>")
-				.concat("</head>")
-				.concat("<body>")
-				.concat("<h1>Заголовок 1</h1>")
-				.concat("<p>Текст параграфа</p>")
-				.concat("</body>")
-				.concat("</html>");
-		
+
+		data = "<!doctype html>".concat("<html>").concat("<head>").concat("<meta charset=\"utf-8\">")
+				.concat("<title>htmlFile</title>").concat("</head>").concat("<body>").concat("<h1>Заголовок 1</h1>")
+				.concat("<p>Текст параграфа</p>").concat("</body>").concat("</html>");
+
 		WrittingFiles htmlFile1 = new WrittingFiles("htmlFile1.html");
-		
+
 		htmlFile1.write(data);
 		htmlFile1.close();
-		
+
 		WrittingFiles csvFile1 = new WrittingFiles("csvFile.csv");
 		data = "Name,age,gender\nSlava,32,male";
 		csvFile1.write(data);
 		csvFile1.close();
-		
-		//CollectionsExample
-		new CollectionsExample();
-		
-		//HashSet example
-		new HashSetExample();
-		
-		//LinkedList example
-		new LinkedListExample();
-		
-		//Log4j
-		logger.trace("Entering application.");
-		
-		Log4jExample l4 = new Log4jExample();
-		
-		if(!l4.doIt()) {
-			logger.error("Did not do it.");
-		}
-		logger.trace("Exiting applicatiopn.");
-				
-		
-		
-		
-		System.out.println("Main class finished the exection ");
 	}
-
 }
