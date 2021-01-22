59ba26c88ffc65037c1aecdbb4c91c41532b8128
slidvo <ks031088@gmail.com>
2021-01-22 19:23:57 +0500

    Showed how to move mouse to elements and change frames.

--- src/test/java/EventListenerAndActions.java
+++ src/test/java/EventListenerAndActions.java
@@ -2,7 +2,9 @@ package test.java;
 
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -14,7 +16,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 @TestMethodOrder(OrderAnnotation.class)
-public class HandlingDragAndDropSlidersResizable extends InitTests {
+public class EventListenerAndActions extends InitTests {
 
 	@BeforeAll
 	static void setup() {
@@ -23,24 +25,24 @@ public class HandlingDragAndDropSlidersResizable extends InitTests {
 
 	@Test
 	@Order(1)
+//	@Ignore
 	void eventFiringWebDriver() {
 		d = new ChromeDriver();
-		
+
 		EventFiringWebDriver driver = new EventFiringWebDriver(d);
-		
+
 		CustomWebListener listener = new CustomWebListener();
-		
+
 		driver.register(listener);
 
 		try {
 			driver.get(googleSearch);
 
 			Thread.sleep(1000);
-			
+
 			driver.findElement(By.xpath("//*[@id=\"fsl\"]/a[1]")).click();
-			
+
 			Thread.sleep(2000);
-			
 
 		} catch (Error e) {
 			e.printStackTrace();
@@ -51,5 +53,49 @@ public class HandlingDragAndDropSlidersResizable extends InitTests {
 		} finally {
 			driver.quit();
 		}
+	}
+
+	@Test
+	@Order(2)
+	void mouseMoving() {
+		d = new ChromeDriver();
+
+		EventFiringWebDriver driver = new EventFiringWebDriver(d);
+
+		CustomWebListener listener = new CustomWebListener();
+
+		driver.register(listener);
+
+		Actions actions = new Actions(driver);
+
+		driver.get(googleSearch);
+
+		try {
+			WebElement elt = driver.findElement(By.xpath("//*[@id=\"gbwa\"]/div/a"));
+			int x = elt.getRect().getX();
+			int y = elt.getRect().getY();
+
+			actions.moveByOffset(x, y).build().perform();
+
+			actions.click(elt).build().perform();
+
+			Thread.sleep(2000);
+			// switch driver to another frame
+			driver.switchTo().frame(driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[3]/iframe")));
+
+			WebElement gmaps = driver
+					.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div/c-wiz/div/div/ul[1]/li[3]/a"));
+
+			actions.moveToElement(gmaps).build().perform();
+
+			actions.click(gmaps).build().perform();
+//			
+			Thread.sleep(2000);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		} finally {
+			driver.quit();
+		}
+
 	}
 }
